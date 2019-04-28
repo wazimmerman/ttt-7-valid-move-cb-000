4e3be06ada41aec660072933d4a3365e6adb4499
William Zimmerman <william.a.zimmerman@gmail.com>
2019-04-28 03:12:02 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -4,7 +4,7 @@ board = [" "," "," "," "," "," "," "," "," "]
 
 def valid_move?(board,index)
   if position_taken?(board, index) == false
-    if index.between(0, 8)
+    if index.between?(0, 8)
       return true
     else
       return false
