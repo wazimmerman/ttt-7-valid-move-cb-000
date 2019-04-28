82a3d2a7614d2143d9e8b21c5f9d75b15125318a
William Zimmerman <william.a.zimmerman@gmail.com>
2019-04-28 03:09:36 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -3,7 +3,7 @@ require_relative './position_taken.rb'
 board = [" "," "," "," "," "," "," "," "," "]
 
 def valid_move?(board,index)
-  if position_taken? = false
+  if position_taken?(board, index) = false
     if index.between(0, 8)
       return true
     else
