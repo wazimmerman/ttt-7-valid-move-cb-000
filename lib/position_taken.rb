f7417b202c5c2f00c7c4b0778038a5f57c70bc10
William Zimmerman <william.a.zimmerman@gmail.com>
2019-04-28 03:12:30 +0000

    Done.

---
+++ lib/position_taken.rb
@@ -0,0 +1,13 @@
+def position_taken?(board, index)
+  if board[index] == "X"
+    return true
+  elsif board[index] == "O"
+    return true
+  elsif board[index] == ""
+    return false
+  elsif board[index] == " "
+    return false
+  elsif board[index] == nil
+    return false
+  end
+end
--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,4 +1,15 @@
-# code your #valid_move? method here
+require_relative './position_taken.rb'
+
+board = [" "," "," "," "," "," "," "," "," "]
+
+def valid_move?(board,index)
+  if position_taken?(board, index) == false
+    if index.between?(0, 8)
+      return true
+    else
+      return false
+    end
+  end
+end 
 
 
-# re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
